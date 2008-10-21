2c28c00179f1fb89106a3b2593975c79384a46cc
bmod@baseoneonline.com <bmod@baseoneonline.com@05cf1d15-2b4d-0410-8d1a-8b58d643e2b8>
2008-10-21 18:31:56 +0000

    Pulsable Blips

---
+++ trunk/basonejlib/src/com/baseoneonline/java/blips/ImagePaintable.java
@@ -0,0 +1,7 @@
+package com.baseoneonline.java.blips;
+
+import java.awt.Graphics2D;
+
+public interface ImagePaintable {
+	public void paint(Graphics2D g, int width, int height);
+}
---
+++ trunk/basonejlib/src/com/baseoneonline/java/blips/Pulsable.java
@@ -0,0 +1,6 @@
+package com.baseoneonline.java.blips;
+
+public interface Pulsable {
+	public void update();
+	public void pulse();
+}
