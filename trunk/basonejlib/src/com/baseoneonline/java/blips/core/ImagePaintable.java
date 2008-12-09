c5dbc4dca5b0a4b9feeb25f4f8c92d745350a434
bmod@baseoneonline.com <bmod@baseoneonline.com@05cf1d15-2b4d-0410-8d1a-8b58d643e2b8>
2008-12-09 10:22:04 +0000

    Removed updated version for blips (is now in blipsgame project)

--- trunk/basonejlib/src/com/baseoneonline/java/blips/core/ImagePaintable.java
+++
@@ -1,7 +0,0 @@
-package com.baseoneonline.java.blips.core;
-
-import java.awt.Graphics2D;
-
-public interface ImagePaintable {
-	public void paint(Graphics2D g, int width, int height);
-}
--- trunk/basonejlib/src/com/baseoneonline/java/blips/sequencer/SequencerListener.java
+++
@@ -1,6 +0,0 @@
-package com.baseoneonline.java.blips.sequencer;
-
-public interface SequencerListener {
-	void onBeat();
-	void onTick();
-}
