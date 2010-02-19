d3b1292abffa5fd6e2a7b8f24fd242402996f041
bmod@baseoneonline.com <bmod@baseoneonline.com@05cf1d15-2b4d-0410-8d1a-8b58d643e2b8>
2010-02-19 09:17:18 +0000

    Moving one level up

--- trunk/basonejlib/src/com/baseoneonline/java/swing/logview/LogRecordFormatter.java
+++
@@ -1,7 +0,0 @@
-package com.baseoneonline.java.swing.logview;
-
-import java.util.logging.LogRecord;
-
-public interface LogRecordFormatter {
-	public String format(LogRecord rec) ;
-}
--- trunk/basonejlib/src/com/baseoneonline/java/test/testMusicOrganizer/SortRule.java
+++
@@ -1,7 +0,0 @@
-package com.baseoneonline.java.test.testMusicOrganizer;
-
-public class SortRule {
-	public String genre = "";
-	public String filenameTemplate;
-	
-}
