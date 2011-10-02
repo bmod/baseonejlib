4b5f4380d98be8a364ed18be422c0b0a0e1aaf28
bmod@baseoneonline.com <bmod@baseoneonline.com@05cf1d15-2b4d-0410-8d1a-8b58d643e2b8>
2011-10-02 00:04:47 +0000

    Cleanup

--- baseonejlib_common/src/com/baseoneonline/java/tools/MayaLink.java
+++ baseonejlib_common/src/com/baseoneonline/java/tools/MayaLink.java
@@ -10,15 +10,13 @@ import java.util.HashMap;
 
 import javax.swing.JOptionPane;
 
-public class MayaLink {
+public class MayaLink
+{
 
-	public enum ReturnType {
+	public enum ReturnType
+	{
 		StringArray, String, Void
 	}
-
-	private static HashMap<ReturnType, String> returnTypeMap;
-
-	private static MayaLink instance;
 
 	private final int port = 20124;
 
@@ -29,12 +27,14 @@ public class MayaLink {
 	private InetSocketAddress host;
 	private PrintWriter writer;
 
-	public MayaLink(final int port) {
+	public MayaLink(final int port)
+	{
 		host = new InetSocketAddress("localhost", port);
-		returnTypeMap = getReturnTypeMap();
+		getReturnTypeMap();
 	}
 
-	private static HashMap<ReturnType, String> getReturnTypeMap() {
+	private static HashMap<ReturnType, String> getReturnTypeMap()
+	{
 		final HashMap<ReturnType, String> map = new HashMap<ReturnType, String>();
 		map.put(ReturnType.Void, "");
 		map.put(ReturnType.StringArray, "string[]");
@@ -42,81 +42,107 @@ public class MayaLink {
 		return map;
 	}
 
-	public int getPort() {
+	public int getPort()
+	{
 		return port;
 	}
 
-	public void setPort(final int port) {
+	public void setPort(final int port)
+	{
 		disconnect();
 		host = new InetSocketAddress(host.getHostName(), port);
 	}
 
-	public String send(final String cmd) {
+	public String send(final String cmd)
+	{
 		connect();
 		getPrintWriter().print(cmd);
 		getPrintWriter().flush();
 		final String re = readFromSocket();
-		if (null != re) return re.trim();
+		if (null != re)
+			return re.trim();
 		return null;
 	}
 
-	private void disconnect() {
-		if (null == sock) return;
-		if (sock.isConnected()) {
-			try {
+	private void disconnect()
+	{
+		if (null == sock)
+			return;
+		if (sock.isConnected())
+		{
+			try
+			{
 				sock.close();
-			} catch (final IOException e) {
+			} catch (final IOException e)
+			{
 				// Don't care
 			}
 		}
 	}
 
-	private String readFromSocket() {
-		try {
-			final InputStreamReader reader = new InputStreamReader(sock
-					.getInputStream());
+	private String readFromSocket()
+	{
+		try
+		{
+			final InputStreamReader reader = new InputStreamReader(
+					sock.getInputStream());
 			final StringBuffer buf = new StringBuffer();
 			int c;
-			while (0 != (c = reader.read())) {
+			while (0 != (c = reader.read()))
+			{
 				buf.append((char) c);
 			}
 			final String re = buf.toString();
-			if (re.startsWith(RE_ERROR)) {
+			if (re.startsWith(RE_ERROR))
+			{
 				// Logger.getLogger(getClass().getName()).severe(
 				// "Maya says: " + re);
-			} else if (re.startsWith(RE_WARN)) {
+			} else if (re.startsWith(RE_WARN))
+			{
 				// Logger.getLogger(getClass().getName()).warning(
 				// "Maya says: " + re);
-			} else {
+			} else
+			{
 				return re;
 			}
-		} catch (final IOException e) {
+		} catch (final IOException e)
+		{
 			throw new RuntimeException(e);
 		}
 		return null;
 	}
 
-	private PrintWriter getPrintWriter() {
-		try {
-			if (null == writer) {
-				writer = new PrintWriter(new OutputStreamWriter(sock
-						.getOutputStream()));
+	private PrintWriter getPrintWriter()
+	{
+		try
+		{
+			if (null == writer)
+			{
+				writer = new PrintWriter(new OutputStreamWriter(
+						sock.getOutputStream()));
 			}
-		} catch (final IOException e) {
+		} catch (final IOException e)
+		{
 			throw new RuntimeException(e);
 		}
 		return writer;
 	}
 
-	private void connect() {
-		if (null == sock) {
+	private void connect()
+	{
+		if (null == sock)
+		{
 			sock = new Socket();
 		}
-		if (!sock.isConnected()) {
-			try {
+		if (!sock.isConnected())
+		{
+			try
+			{
 				sock.connect(host);
-			} catch (final IOException e) {
-				JOptionPane.showMessageDialog(null, "Error connecting to maya, try: commandPort -n \"localhost:"
+			} catch (final IOException e)
+			{
+				JOptionPane.showMessageDialog(null,
+						"Error connecting to maya, try: commandPort -n \"localhost:"
 								+ port + "\";");
 			}
 		}
