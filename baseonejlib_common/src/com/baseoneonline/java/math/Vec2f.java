926f1f637296fea2ee12b58b0f93051cf50c1c54
bmod <bmod@baseoneonline.com>
2012-08-04 09:33:43 +0000

    Added DouglasPeucker polyline reducer

--- baseonejlib_common/src/com/baseoneonline/java/math/Vec2f.java
+++ baseonejlib_common/src/com/baseoneonline/java/math/Vec2f.java
@@ -1,47 +1,74 @@
 package com.baseoneonline.java.math;
 
-public class Vec2f {
+public class Vec2f
+{
 
 	public float x;
 	public float y;
 
-	public Vec2f(final float x, final float y) {
+	public Vec2f(final float x, final float y)
+	{
 		this.x = x;
 		this.y = y;
 	}
 
-	public Vec2f() {
+	public Vec2f()
+	{
 		x = 0;
 		y = 0;
 	}
 
-	public Vec2f mult(final float n) {
+	public Vec2f mult(final float n)
+	{
 		return new Vec2f(x * n, y * n);
 	}
 
-	public void zero() {
+	public void zero()
+	{
 		x = 0;
 		y = 0;
 	}
 
-	public void addLocal(final Vec2f v) {
+	public void addLocal(final Vec2f v)
+	{
 		x += v.x;
 		y += v.y;
 	}
 
-	public Vec2f set(final Vec2f v) {
+	public Vec2f set(final Vec2f v)
+	{
 		x = v.x;
 		y = v.y;
 		return this;
 	}
 
-	public void divideLocal(final float valSum) {
+	public Vec2f minus(Vec2f v)
+	{
+		return new Vec2f(x - v.x, y - v.y);
+	}
+
+	public void divideLocal(final float valSum)
+	{
 		x /= valSum;
 		y /= valSum;
 	}
 
+	public float dot(Vec2f v)
+	{
+		return x * v.x + y * v.y;
+	}
+
+	public float distanceSquared(Vec2f v)
+	{
+		float dx = v.x - x;
+		float dy = v.y - y;
+
+		return dx * dx + dy * dy;
+	}
+
 	@Override
-	public String toString() {
+	public String toString()
+	{
 		return "[Vec2f x:" + x + ", y:" + y + "]";
 	}
 
