5fe522b393c420eba91c5ded036498d3bd1697fd
bmodone@gmail.com <bmodone@gmail.com@05cf1d15-2b4d-0410-8d1a-8b58d643e2b8>
2012-12-24 00:37:01 +0000

    Simple asset manager

--- baseonejlib_ardor3d/src/com/baseoneonline/jlib/ardor3d/AssetManager.java
+++ baseonejlib_ardor3d/src/com/baseoneonline/jlib/ardor3d/AssetManager.java
@@ -1,60 +1,51 @@
 package com.baseoneonline.jlib.ardor3d;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.HashMap;
 
 import com.ardor3d.extension.model.collada.jdom.ColladaImporter;
 import com.ardor3d.extension.model.collada.jdom.data.ColladaStorage;
 import com.ardor3d.math.MathUtils;
 import com.ardor3d.math.Matrix3;
 import com.ardor3d.math.Vector3;
-import com.ardor3d.util.resource.ResourceLocatorTool;
 
 public class AssetManager {
 
+	private final Class<?> refClass;
 	private ColladaImporter colladaImporter;
-	private final URI baseDir;
-	private final HashMap<String, Object> assets = new HashMap<>();
 
-	public AssetManager(ClassLoader classLoader) {
-		try {
-			this.baseDir = classLoader.getResource("").toURI();
-		} catch (URISyntaxException e) {
-			throw new RuntimeException(e);
-		}
-		// Logger.getLogger("com.ardor3d.extension.model.collada").setLevel(
-		// Level.SEVERE);
+	private static final Matrix3 Z_UP_CORRECTION = new Matrix3().fromAngles(
+			-MathUtils.PI / 2, 0, 0);
 
+	/**
+	 * @param classLoader
+	 *            Will be used for path reference.
+	 */
+	public AssetManager(Class<?> refClass) {
+		this.refClass = refClass;
 	}
 
 	@SuppressWarnings("unchecked")
-	public <T> T load(String resource, Class<T> type) {
+	public <T> T load(String resource, Class<T> returnType) {
 
-		if (assets.containsKey(resource))
+		if (resource.toLowerCase().endsWith(".dae"))
+			return (T) loadCollada(resource);
 
-			if (resource.toLowerCase().endsWith(".dae"))
-				return (T) loadCollada(resource);
-
-		throw new RuntimeException("No support for '" + resource + "'.");
+		throw new UnsupportedOperationException(String.format(
+				"No loader for resource '%s' of type '%s'", resource,
+				returnType.getName()));
 	}
 
 	private ColladaStorage loadCollada(String resource) {
-
 		if (colladaImporter == null) {
 			colladaImporter = new ColladaImporter();
+
 		}
 		try {
-			ColladaStorage stor = colladaImporter.load(ResourceLocatorTool
-					.locateResource(ResourceLocatorTool.TYPE_MODEL, resource));
+			ColladaStorage stor = colladaImporter.load(resource);
 
 			// Correct for Z-up axis
-			if (stor.getAssetData().getUpAxis().equals(Vector3.UNIT_Z)) {
-				stor.getScene().setRotation(
-						new Matrix3().fromAngles(-MathUtils.HALF_PI, 0, 0));
-			}
-
+			if (stor.getAssetData().getUpAxis().equals(Vector3.UNIT_Z))
+				stor.getScene().setRotation(Z_UP_CORRECTION);
 			return stor;
 		} catch (IOException e) {
 			throw new RuntimeException(e);
