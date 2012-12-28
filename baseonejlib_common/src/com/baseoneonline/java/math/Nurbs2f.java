74e02b29359ac0e50f00114aa4a93199da0ac3b8
bmodone <bmodone@gmail.com>
2012-12-28 12:28:49 +0000

    Removed curve abstraction

--- baseonejlib_common/src/com/baseoneonline/java/math/Nurbs2f.java
+++ baseonejlib_common/src/com/baseoneonline/java/math/Nurbs2f.java
@@ -3,36 +3,34 @@ package com.baseoneonline.java.math;
 /**
  * 
  */
-public class Nurbs2f extends Curve<Vec2f, Vec2f>
-{
+public class Nurbs2f {
+	public enum Mode {
+		Open, Loop, Clamp
+	}
 
 	private Vec2f[] cvs;
 	private double[] knots;
 	private final int degree = 3;
 	private int order;
 	private double maxKnotValue;
+	protected Mode mode = Mode.Open;
 
-	public Nurbs2f(final Vec2f[] cvs)
-	{
-		super(cvs);
+	public Nurbs2f(final Vec2f[] cvs) {
+		setCVs(cvs);
 	}
 
-	@Override
-	public void setCVs(final Vec2f[] cvs)
-	{
-		super.setCVs(cvs);
+	public void setCVs(final Vec2f[] cvs) {
+		this.cvs = cvs;
 		rebuild();
 	}
 
-	private void rebuild()
-	{
+	private void rebuild() {
 		order = degree + 1;
 		final int numKnots = cvs.length + order;
 
 		knots = new double[numKnots];
 		float v = 0;
-		for (int i = 0; i < numKnots; i++)
-		{
+		for (int i = 0; i < numKnots; i++) {
 			knots[i] = v;
 			if (i >= degree && i < numKnots - degree - 1)
 				v += 1;
@@ -40,9 +38,7 @@ public class Nurbs2f extends Curve<Vec2f, Vec2f>
 		maxKnotValue = v;
 	}
 
-	@Override
-	public Vec2f getPoint(final double t, Vec2f store)
-	{
+	public Vec2f getPoint(final double t, Vec2f store) {
 		if (null == store)
 			store = new Vec2f();
 
@@ -56,8 +52,7 @@ public class Nurbs2f extends Curve<Vec2f, Vec2f>
 		float valSum = 0;
 		float v = 0;
 		final Vec2f tmp = new Vec2f();
-		for (int i = 0; i < cvs.length; i++)
-		{
+		for (int i = 0; i < cvs.length; i++) {
 			v = (float) CurveAlgorithms.coxDeBoor(knots, t * maxKnotValue, i,
 					degree);
 			valSum += v;
@@ -70,9 +65,31 @@ public class Nurbs2f extends Curve<Vec2f, Vec2f>
 		return store;
 	}
 
-	@Override
-	public Vec2f getVelocity(double t, Vec2f store)
-	{
+	public Vec2f getVelocity(double t, Vec2f store) {
 		throw new RuntimeException("Not implemented yet.");
+	}
+
+	public Mode getMode() {
+		return mode;
+	}
+
+	public void setMode(Mode mode) {
+		this.mode = mode;
+	}
+
+	/**
+	 * @param i
+	 *            Index of the control vertex to retrieve.
+	 * @return Instance of a control vertex used by this curve.
+	 */
+	public Vec2f getCV(int i) {
+		return cvs[i];
+	}
+
+	/**
+	 * @return The number of control vertices in this curve.
+	 */
+	public int getCVCount() {
+		return cvs.length;
 	}
 }
