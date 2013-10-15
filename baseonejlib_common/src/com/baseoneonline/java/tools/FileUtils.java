14f2f0a7c4d74629717a043bc7d4f4e95c6eb364
bmodone <bmodone@gmail.com>
2013-10-15 20:25:22 +0000

    Added convenience method replaceExtension(File)

--- baseonejlib_common/src/com/baseoneonline/java/tools/FileUtils.java
+++ baseonejlib_common/src/com/baseoneonline/java/tools/FileUtils.java
@@ -26,25 +26,20 @@ public class FileUtils {
 	 * @return {@link String} containing the contents of the file or null if the
 	 *         file could not be read.
 	 */
-	public static String readFile(final File f)
-	{
-		try
-		{
+	public static String readFile(final File f) {
+		try {
 			final FileReader reader = new FileReader(f);
 			final StringBuffer buf = new StringBuffer();
 			int c;
-			while ((c = reader.read()) != -1)
-			{
+			while ((c = reader.read()) != -1) {
 				buf.append((char) c);
 			}
 			reader.close();
 			return buf.toString();
-		} catch (final FileNotFoundException e)
-		{
+		} catch (final FileNotFoundException e) {
 			Logger.getLogger(StringUtils.class.getName()).warning(
 					"File not found: " + f.getAbsolutePath());
-		} catch (final IOException e)
-		{
+		} catch (final IOException e) {
 			Logger.getLogger(StringUtils.class.getName()).warning(
 					"IO Exception while reading file: " + f.getAbsolutePath());
 			e.printStackTrace();
@@ -60,16 +55,13 @@ public class FileUtils {
 	 * @param s
 	 *            Write this {@link String} to the file.
 	 */
-	public static void writeFile(final File f, final String s)
-	{
-		try
-		{
+	public static void writeFile(final File f, final String s) {
+		try {
 			final FileWriter writer = new FileWriter(f);
 			writer.write(s);
 			writer.flush();
 			writer.close();
-		} catch (final IOException e)
-		{
+		} catch (final IOException e) {
 			Logger.getLogger(StringUtils.class.getName()).warning(
 					"IO Exception while writing: " + f.getAbsolutePath());
 		}
@@ -79,21 +71,15 @@ public class FileUtils {
 			.toCharArray();
 
 	public static String removeIllegalChars(final String filename,
-			final boolean allowSeparators)
-	{
+			final boolean allowSeparators) {
 		final StringBuffer buf = new StringBuffer();
 		final char[] fname = filename.toCharArray();
-		for (final char fc : fname)
-		{
-			if (allowSeparators && (fc == '/' || fc == '\\'))
-			{
+		for (final char fc : fname) {
+			if (allowSeparators && (fc == '/' || fc == '\\')) {
 				buf.append(fc);
-			} else
-			{
-				for (final char c : ALLOWED_CHARS)
-				{
-					if (fc == c)
-					{
+			} else {
+				for (final char c : ALLOWED_CHARS) {
+					if (fc == c) {
 						buf.append(c);
 						break;
 					}
@@ -103,21 +89,17 @@ public class FileUtils {
 		return buf.toString();
 	}
 
-	public static String readFile(final URL url)
-	{
-		try
-		{
+	public static String readFile(final URL url) {
+		try {
 			final BufferedInputStream bin = new BufferedInputStream(
 					url.openStream());
 			final StringBuffer buf = new StringBuffer();
 			int c;
-			while ((c = bin.read()) != -1)
-			{
+			while ((c = bin.read()) != -1) {
 				buf.append((char) c);
 			}
 			return buf.toString();
-		} catch (final IOException e)
-		{
+		} catch (final IOException e) {
 			Logger.getLogger(FileUtils.class.getName()).severe(
 					"Error loading: " + url);
 		}
@@ -129,14 +111,12 @@ public class FileUtils {
 	 * @param directory
 	 * @return True if the specified file is inside the specified directory
 	 */
-	public static boolean isChildOf(final File f, final File directory)
-	{
+	public static boolean isChildOf(final File f, final File directory) {
 		if (!directory.isDirectory())
 			return false;
 
 		File current = f;
-		while (true)
-		{
+		while (true) {
 			final File parent = current.getParentFile();
 			if (null == parent)
 				return false;
@@ -156,27 +136,22 @@ public class FileUtils {
 	 * @return
 	 */
 	public static List<File> findEmpty(final File directory,
-			final DisposableFilter filter)
-	{
+			final DisposableFilter filter) {
 		final List<File> stor = new ArrayList<File>();
 		FileUtils.findEmpty(directory, stor, filter);
 		return stor;
 	}
 
 	public static void findEmpty(final File parent, final List<File> stor,
-			final DisposableFilter filter)
-	{
-		if (FileUtils.isEmpty(parent, filter))
-		{
+			final DisposableFilter filter) {
+		if (FileUtils.isEmpty(parent, filter)) {
 			stor.add(parent);
 			return;
 		}
 
-		for (final File dir : parent.listFiles())
-		{
+		for (final File dir : parent.listFiles()) {
 
-			if (dir.isDirectory())
-			{
+			if (dir.isDirectory()) {
 				if (FileUtils.isEmpty(dir, filter))
 					stor.add(dir);
 				else
@@ -203,13 +178,11 @@ public class FileUtils {
 	 *         only empty directories.
 	 */
 	public static boolean isEmpty(final File dir,
-			final DisposableFilter disposableFilter)
-	{
+			final DisposableFilter disposableFilter) {
 		if (dir.isFile())
 			return false;
 
-		for (final File f : dir.listFiles())
-		{
+		for (final File f : dir.listFiles()) {
 			if (f.isFile() && !disposableFilter.isDisposable(f))
 				return false;
 
@@ -237,57 +210,50 @@ public class FileUtils {
 		public void onFile(File f);
 	}
 
-	public static File canonical(final Object... args) throws IOException
-	{
+	public static File canonical(final Object... args) throws IOException {
 		final String[] elements = new String[args.length];
-		for (int i = 0; i < args.length; i++)
-		{
+		for (int i = 0; i < args.length; i++) {
 			final Object o = args[i];
-			if (o instanceof Character)
-			{
+			if (o instanceof Character) {
 				elements[i] = Character.toString((Character) o);
-			} else if (o instanceof String)
-			{
+			} else if (o instanceof String) {
 				elements[i] = ((String) o).trim();
-			} else if (o instanceof File)
-			{
+			} else if (o instanceof File) {
 				elements[i] = ((File) o).getAbsolutePath();
 			}
 		}
 		return new File(StringUtils.join(elements, "")).getCanonicalFile();
 	}
 
-	public static File relative(final File f, final File base)
-	{
+	public static File relative(final File f, final File base) {
 		return new File(f.toURI().relativize(base.toURI()).getPath());
 	}
 
 	public static String replaceExtension(final String filename,
-			final String rep)
-	{
-		return FilenameUtils.getBaseName(filename) + "." + rep;
+			final String rep) {
+		return FilenameUtils.removeExtension(filename) + "." + rep;
 	}
 
-	public static File file(final String... args)
-	{
+	public static File file(final String... args) {
 		return new File(StringUtils.join(args, "/"));
 	}
 
-	public static File createTempDir(final File baseDir)
-	{
+	public static File createTempDir(final File baseDir) {
 		final String baseName = System.currentTimeMillis() + "-";
 
-		for (int counter = 0; counter < TEMP_DIR_ATTEMPTS; counter++)
-		{
+		for (int counter = 0; counter < TEMP_DIR_ATTEMPTS; counter++) {
 			final File tempDir = new File(baseDir, baseName + counter);
-			if (tempDir.mkdir())
-			{
+			if (tempDir.mkdir()) {
 				return tempDir;
 			}
 		}
 		throw new IllegalStateException("Failed to create directory within "
 				+ TEMP_DIR_ATTEMPTS + " attempts (tried " + baseName + "0 to "
 				+ baseName + (TEMP_DIR_ATTEMPTS - 1) + ')');
+	}
+
+	public static File replaceExtension(File file, String ext) {
+		return new File(replaceExtension(file.getAbsolutePath(), ext));
 	}
 
 }
