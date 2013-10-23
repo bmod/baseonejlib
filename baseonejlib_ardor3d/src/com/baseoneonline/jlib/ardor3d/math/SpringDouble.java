87f9f75ec3075d7d75bd9d62f38cdd7ab91f3efb
bmodone@gmail.com <bmodone@gmail.com@05cf1d15-2b4d-0410-8d1a-8b58d643e2b8>
2013-10-23 23:35:18 +0000

    Some really interesting refactorings and small bug fixes

---
+++ baseonejlib_ardor3d/src/com/baseoneonline/jlib/ardor3d/math/SpringDouble.java
@@ -0,0 +1,9 @@
+package com.baseoneonline.jlib.ardor3d.math;
+
+import java.util.logging.Logger;
+
+public class SpringDouble extends SmoothDouble {
+	public SpringDouble() {
+		Logger.getLogger(getClass().getName()).warning("Please implement!");
+	}
+}
